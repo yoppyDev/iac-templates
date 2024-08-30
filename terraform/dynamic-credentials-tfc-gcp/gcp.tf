161fa750915348c22724377f15829a03256ae490
yoppytaro <yoppygood1201@docomo.ne.jp>
2024-08-31 07:58:28 +0900

    #13 Terraform CloudからGoogle Cloudへの動的クレデンシャル設定

--- terraform/dynamic-credentials-tfc-gcp/gcp.tf
+++ terraform/dynamic-credentials-tfc-gcp/gcp.tf
@@ -3,21 +3,23 @@ provider "google" {
   region  = "global"
 }
 
-data "google_project" "project" {
-}
-
+# GCPサービスを有効にするリソース
 resource "google_project_service" "services" {
-  count   = length(var.gcp_service_list)
-  service = var.gcp_service_list[count.index]
+  for_each           = toset(var.gcp_service_list)
+  service            = each.value
+  disable_on_destroy = false
 }
 
+# Workload Identity Poolの作成
 resource "google_iam_workload_identity_pool" "tfc_pool" {
-  workload_identity_pool_id = "my-tfc-pool"
+  workload_identity_pool_id = "tfc-pool"
 }
 
+# Workload Identity Pool Providerの作成
 resource "google_iam_workload_identity_pool_provider" "tfc_provider" {
   workload_identity_pool_id          = google_iam_workload_identity_pool.tfc_pool.workload_identity_pool_id
   workload_identity_pool_provider_id = "my-tfc-provider-id"
+
   attribute_mapping = {
     "google.subject"                        = "assertion.sub",
     "attribute.aud"                         = "assertion.aud",
@@ -31,23 +33,28 @@ resource "google_iam_workload_identity_pool_provider" "tfc_provider" {
     "attribute.terraform_run_id"            = "assertion.terraform_run_id",
     "attribute.terraform_full_workspace"    = "assertion.terraform_full_workspace",
   }
+
   oidc {
     issuer_uri = "https://${var.tfc_hostname}"
   }
+
   attribute_condition = "assertion.sub.startsWith(\"organization:${var.tfc_organization_name}:project:${var.tfc_project_name}:workspace:${var.tfc_workspace_name}\")"
 }
 
+# サービスアカウントの作成
 resource "google_service_account" "tfc_service_account" {
   account_id   = "tfc-service-account"
   display_name = "Terraform Cloud Service Account"
 }
 
+# サービスアカウントにWorkload Identity Userロールを割り当て
 resource "google_service_account_iam_member" "tfc_service_account_member" {
   service_account_id = google_service_account.tfc_service_account.name
   role               = "roles/iam.workloadIdentityUser"
   member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.tfc_pool.name}/*"
 }
 
+# サービスアカウントにプロジェクトエディターロールを割り当て
 resource "google_project_iam_member" "tfc_project_member" {
   project = var.gcp_project_id
   role    = "roles/editor"
